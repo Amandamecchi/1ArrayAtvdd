1f160b542cd1e1e04144b5bdf0f8f7f1a40f902f
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-03-17 20:57:24 -0300

    Sexto método: Includes

--- metodos4.js
+++ metodos4.js
@@ -8,18 +8,5 @@ console.log( [1, 2, 3, NaN].includes( NaN ) ) //poderia ser uma constante,
 //mas o .includes ficou responsável por verificar se 2 está aqui
 // True/False 
 
-console.log( [1, 2, 3].includes( 5 ) ) 
+console.log( [1, 2, 3].includes( 5 ) );
 
-
-//MÉTODOS ARRAY Aula 14-03-2024
-//Método Push - Adiciona um ou mais elementos ao final de um array
-//Sintaxe: array.push();
-let Notas = [5, 8, 5, 7];
-//Testes:
-
-Notas.push( 10 );
-console.log(Notas);
-
-
-Notas.push(9, 5, 6);
-console.log(Notas);
