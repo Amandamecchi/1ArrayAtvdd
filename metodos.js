9e77a43b0e60c3e47eb8c51d7faabb698ac9748c
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-03-17 19:59:08 -0300

    Segundo Método: Pop

---
+++ metodos.js
@@ -0,0 +1,13 @@
+//MÉTODOS ARRAY Aula 14-03-2024
+//Método .Pop()- É utilizado para remover o último elemento do array
+ //Sintaxe: array.pop() // Sorvetes.pop()
+
+ let Sorvetes = ['Chocolate', 'Baunilia', 'Morango'];
+
+ //Testes: 
+
+ let UltimoSorvete = Sorvetes.pop();
+ console.log(Sorvetes);
+
+ let UltimoSorvete2 = Sorvetes.pop('Morango');
+ console.log(UltimoSorvete2);
