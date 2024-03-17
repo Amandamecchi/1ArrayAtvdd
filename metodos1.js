68e2881b7aa08f0592b25ccf971a7c997ce6876d
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-03-17 20:03:41 -0300

    Terceiro método: Join

---
+++ metodos1.js
@@ -0,0 +1,13 @@
+//Métodos Array - Aula 14-03-2024
+ //Método Array.join - Responsável por juntar os itens de um array em uma única string 
+// sintaxe: array.join()
+
+let paises = ['Romenia', 'Usa', 'Brazil'];
+
+//Testes:
+
+let res = paises.join(' ');
+console.log(res)
+
+let res2 = paises.join('-');
+console.log(res2);
