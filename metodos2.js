07aea121db75926a494aed4b1f93aeae95a410fd
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-03-17 20:07:45 -0300

    Quarto  método: Unshift

---
+++ metodos2.js
@@ -0,0 +1,16 @@
+//MÉTODOS ARRAY Aula 14-03-2024
+//Método unshift() - Adiciona um ou mais elementos no início de um array
+//sintaxe: array.unshift()
+
+let números = [1, 2, 3, 4];
+
+//Testes: 
+
+números.unshift(0);
+console.log(números);
+
+números.unshift(-1, -2, -3, -4);
+console.log(números);
+
+
+
