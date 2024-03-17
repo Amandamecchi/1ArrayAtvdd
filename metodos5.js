10bd85225c38504a91c8e236595112cb4e36a81c
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-03-17 20:37:13 -0300

    Sétimo método: Shift

--- metodos5.js
+++ metodos5.js
@@ -14,19 +14,3 @@ console.log(removedElement);
 let removedElement2 = numbers.shift( 2 );
 console.log(removedElement2);
 //Removeu o 80 e 90
-
-
-//MÉTODOS ARRAY Aula 14-03-2024 
-//Método splice() - Altera o conteúdo de uma lista 
-//Sintaxe: array.splice (itemn, quantidadeRemovida)
-
-let Nomes = ['Maria', 'João', 'Valentina', 'Enzo'];
-
-//Testes:
-
-Nomes.splice(2, 1); //2= a partir da posição que deseja remover 1= quero remover apenas só 1 elemento
-console.log(Nomes); 
-
-
-Nomes.splice(2, 1, 'Júlia') //removeu Enzo e adcionou Júlia
-console.log(Nomes); 
