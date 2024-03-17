b673a8c58147f63c0d43b58d762a8ec77dc92eef
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-03-17 20:14:57 -0300

    Sexto  método: Includes

---
+++ metodos4.js
@@ -0,0 +1,17 @@
+//MÉTODOS ARRAY Aula 14-03-2024
+//Método includes - é utilizado para saber se um array possui determinado valor ou elemento
+//Sintaxe: Array.includes()
+
+//Testes:
+
+console.log( [1, 2, 3, NaN].includes( NaN ) ) //poderia ser uma constante,  
+//mas o .includes ficou responsável por verificar se 2 está aqui
+// True/False 
+
+console.log( [1, 2, 3].includes( 5 ) ) 
+
+
+//MÉTODOS ARRAY Aula 14-03-2024
+//Método Push - Adiciona um ou mais elementos ao final de um array
+//Sintaxe: array.push();
+let Notas = [5, 8, 5, 7];
