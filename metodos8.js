202257960894595486e620b09badc638164bffe2
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-03-17 20:59:35 -0300

    Nono método: Push

---
+++ metodos8.js
@@ -0,0 +1,14 @@
+//MÉTODOS ARRAY Aula 14-03-2024
+//Método Push - Adiciona um ou mais elementos ao final de um array
+//Sintaxe: array.push();
+let Notas = [5, 8, 5, 7];
+
+//Testes:
+
+Notas.push( 10 );
+console.log(Notas);
+
+
+Notas.push(9, 5, 6);
+console.log(Notas);
+
