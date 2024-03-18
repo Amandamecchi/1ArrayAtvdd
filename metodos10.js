a1149024db0a5f3c609d8a3ce231b81bb4d26b17
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-03-17 21:04:38 -0300

    Décimo método: IndexOF

---
+++ metodos10.js
@@ -0,0 +1,16 @@
+//MÉTODOS ARRAY Aula 14-03-2024 
+//Método indexOf() - recupera a posição primeira ocorrencia de um elemento em uma string, 
+//se o valor não existir ele retorna o valor -1 
+//Sintaxe: array.indexOf(elemento)
+
+let Letras = ['a', 'b', 'c', 'd', 'e'];
+
+//Teste:
+
+let idx = Letras.indexOf('b');
+Letras [idx] = 'e';
+console.log(Letras);
+
+
+Letras [idx] = 'W';
+console.log(idx);
